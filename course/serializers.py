9a4a5308e711a3438f28afa9c55bbb631624b826
Danila <icy.calmness@gmail.com>
2023-10-01 14:10:07 +0300

    extend class CourseSerializer (add field lessons, lessons_count); course.serializers.py

--- course/serializers.py
+++ course/serializers.py
@@ -3,13 +3,26 @@ from rest_framework import serializers
 from course.models import *
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()  # поле вывода количества уроков в курсе
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True, )  # поле вывода уроков курса
+
+    def get_lessons_count(self, instance):
+        # метод определяет количество уроков в курсе
+        lessons = Lesson.objects.filter(course=instance).all()
+        if lessons:
+            return lessons.count()
+        return 0
+
+    class Meta:
+        model = Course
+        fields = ('id', 'owner', 'name', 'preview', 'description', 'lessons', 'lessons_count',)
+
+
+
