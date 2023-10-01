fd3063f94156e76f69e411184df9e339ae03bfb5
Danila <icy.calmness@gmail.com>
2023-10-01 18:52:17 +0300

    extend class UserSerializer (add all_payment);

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from users.models import User
+from course.models import Payment
+
+
+class UserPaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
+    all_payment = UserPaymentSerializer(source='payment_set', many=True, read_only=True,)
+
     class Meta:
         model = User
-        fields = ('email', 'phone', 'city',)
+        fields = ('id', 'email', 'phone', 'city', 'all_payment',)
